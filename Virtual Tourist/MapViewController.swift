c3b5bc4cfb785fd16cab67bee3846c31214d3dd6
Kemal Can Kaynak <kcankaynak@gmail.com>
2020-07-05 17:42:23 +0200

    Project created

---
+++ Virtual Tourist/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Kemal Kaynak on 05.07.20.
+//  Copyright © 2020 Kemal Kaynak. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
